c356476c91e7df5f6ffec57e61d265f758d6eeaf
Masood Seddighin <seddighin.masood@gmail.com>
2012-12-12 17:35:31 +0330

    refs #1971 @3h10m بررسی و اضافه نمودن select2

--- src/arshlib__user_mail/Output.py
+++ src/arshlib__user_mail/Output.py
@@ -5,61 +5,61 @@ class AppTest(TestCase):
 		self.assertTrue(self.client.login(username='admin',password='admin'))
 
 	def testmail__compose(self):
-		#view function = <function compose at 0x1083a1500>
+		#view function = <function compose at 0x10cca39b0>
 		response = self.client.get('mail/compose')
 		self.assertEqual(response.status_code,200)
 		self.assertTemplateUsed(response,'')
 
 	def testmail__search(self):
-		#view function = <function search at 0x1083a1668>
+		#view function = <function search at 0x10cca3b18>
 		response = self.client.get('mail/search/')
 		self.assertEqual(response.status_code,200)
 		self.assertTemplateUsed(response,'')
 
 	def testmail__home(self):
-		#view function = <function see at 0x1083a1398>
+		#view function = <function see at 0x10cca3848>
 		response = self.client.get('mail/view/')
 		self.assertEqual(response.status_code,200)
 		self.assertTemplateUsed(response,'')
 
 	def testmail__inbox_archive(self):
-		#view function = <function see at 0x1083a1398>
+		#view function = <function see at 0x10cca3848>
 		response = self.client.get('mail/view/archive/')
 		self.assertEqual(response.status_code,200)
 		self.assertTemplateUsed(response,'')
 
 	def testmail__see_label(self):
-		#view function = <function see at 0x1083a1398>
+		#view function = <function see at 0x10cca3848>
 		response = self.client.get('mail/view/(?P<label_slug[a-zA-Z0-9_\.]+)/')
 		self.assertEqual(response.status_code,200)
 		self.assertTemplateUsed(response,'')
 
 	def testmail__see_label_archive(self):
-		#view function = <function see at 0x1083a1398>
+		#view function = <function see at 0x10cca3848>
 		response = self.client.get('mail/view/(?P<label_slug[a-zA-Z0-9_\.]+)/archive')
 		self.assertEqual(response.status_code,200)
 		self.assertTemplateUsed(response,'')
 
 	def testmail__see_thread(self):
-		#view function = <function see at 0x1083a1398>
+		#view function = <function see at 0x10cca3848>
 		response = self.client.get('mail/view/(?P<label_slug[a-zA-Z0-9_\.]+)/(?P<thread_slug[a-zA-Z0-9_\.]+)/')
 		self.assertEqual(response.status_code,200)
 		self.assertTemplateUsed(response,'')
 
 	def testmail__see_thread_direct(self):
-		#view function = <function see at 0x1083a1398>
+		#view function = <function see at 0x10cca3848>
 		response = self.client.get('mail/threads/view/(?P<thread_slug[a-zA-Z0-9_\.]+)/')
 		self.assertEqual(response.status_code,200)
 		self.assertTemplateUsed(response,'')
 
 	def testmail__thread__mark(self):
-		#view function = <function mark_thread at 0x1083a1938>
+		#view function = <function mark_thread at 0x10cca3de8>
 		response = self.client.get('mail/threads/(?P<thread_slug[a-zA-Z0-9_\.]+)/mark_(?P<actionread|unread)')
 		self.assertEqual(response.status_code,200)
 		self.assertTemplateUsed(response,'')
 
 	def testsetup(self):
-		#view function = <function setup at 0x1083a1230>
+		#view function = <function setup at 0x10cca36e0>
 		response = self.client.get('mail/setup')
 		self.assertEqual(response.status_code,200)
 		self.assertTemplateUsed(response,'')
