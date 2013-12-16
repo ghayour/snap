36a9c2d8f3d299d3d0fcb15dacc42dfb80069300
AmirAli Akbari <akbari@arsh.co>
2013-12-16 11:36:25 +0330

    رفع موقتی عیب نمایش نام فایل

--- templatetags/my_extras.py
+++ templatetags/my_extras.py
@@ -1,11 +1,17 @@
 #coding= utf-8
 import os
+import base64
+
 from django import template
 
 register = template.Library()
 
+
 @register.filter
 def filename(value):
-    import base64
-    filename=os.path.basename(value.file.name).split('.')[0]
-    return unicode(base64.urlsafe_b64decode(str(filename)), 'utf-8')
+    filename = os.path.basename(value.file.name).split('.')[0]
+    try:
+        return unicode(base64.urlsafe_b64decode(str(filename)))
+    except TypeError:
+        # TODO: check this!
+        return filename
