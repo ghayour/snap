8c1770c9f044aa2361d94b9a74bfbbd7e3e1c84f
barzekar <barzekar@arsh.co>
2013-12-26 01:44:11 +0330

    استفاده از bootboxبرای confirm

--- static/mail/addressbook/editable.js
+++ static/mail/addressbook/editable.js
@@ -4,26 +4,25 @@
 
 $(document).ready(function(){
 
-    $('.confirmModal').confirmModal({
-        confirmTitle     : 'هشدار',
-        confirmMessage   : 'آیا مطمئنید که میخواهید این حساب را حذف کنید ؟ ',
-        confirmOk        : 'بله',
-        confirmCancel    : 'خیر',
-        confirmDirection : 'rtl',
-        confirmStyle     : 'primary',
-        confirmCallback :  function(){
+    $('.confirmModal').click(function(){
+        var pk = $(this).data("pk");
+        bootbox.confirm('آیا شما مطمئنید که می خواهید این حساب را حذف کنید  ؟',
+        function(result){
+            if (result){
             $.ajax({
-                 url : '/view/addressbook',
-                type : 'POST',
-                data : { pk : this.pk},
-                success:function(){
-                    ajaxLoader.show();
-                    window.location.reload();
-                }
-                });
+            url : '/view/addressbook',
+            type : 'POST',
+            data : { pk : pk },
+            success:function(){
+                ajaxLoader.show();
+                window.location.reload();
+            }
 
+            })
         }
+            else{};
 
-});
+        });
+    });
+ });
 
-});
--- static/mail/managelabel/managelabel.js
+++ static/mail/managelabel/managelabel.js
@@ -1,55 +1,37 @@
 $(document).ready(function(){
    $.fn.editable.defaults.mode = 'inline';
 
-//   $('td .glyphicon-trash').click(function(){
-//       var cur_tr = $(this).parents('tr');
-//       var doIt = confirm("آیا می خواهید این برچسب را حذف کنید ؟ ");
-//       if (doIt){
-//       $.ajax({
-//           url : '/manage-label/',
-//           type : 'POST',
-//           data : {
-//               pk : $(this).parent().parent().attr('id'),
-//               name : 'delete'
-//           },
-//           success:function(){
-////                ajaxLoader.show();
-////                window.location.reload();
-//                cur_tr.remove();
-//                parent.document.getElementById("sidebar").reload();
-//
-//           }
-//       });
-//
-//       }
-//        });
-
-    $('.confirmModal').confirmModal({
-        confirmTitle     : 'هشدار',
-        confirmMessage   : 'آیا مطمئنید که می خواهید این برچسب را حذف کنید ؟ ',
-        confirmOk        : 'بله',
-        confirmCancel    : 'خیر',
-        confirmDirection : 'rtl',
-        confirmStyle     : 'primary',
-        confirmCallback  : function(){
-            $.ajax({
-                url : '/manage-label/',
-           type : 'POST',
-           data : {
-               pk : this.pk,
-               name : 'delete'
-           },
-           success:function(){
-//                cur_tr.remove();
-               $('tr #'+ this.pk).remove();
-                ajaxLoader.show();
-                window.location.reload();
-           }
-            })
-        }
-    });
 
     $('td .glyphicon-edit').click(function(){
        $(this).parents('tr').find('.name').editable('toggleDisabled');
         });
 });
+
+$(document).ready(function(){
+    $('.confirmModal').click(function(){
+        var cur_tr = $(this).parents('tr');
+        var pk = $(this).parent().parent().attr('id');
+        bootbox.confirm('are you sure',
+            function(result){
+
+            if (result){
+                $.ajax({
+                url  : '/manage-label/',
+                type : 'POST',
+                data : {
+                    pk   : pk ,
+                    name : 'delete'},
+                success:function(){
+                    cur_tr.remove();
+                    ajaxLoader.show();
+                    window.location.reload()
+                }
+
+
+            })//ajax
+
+    }
+            else{}
+            })//bootbox
+    }); // .confirmModal
+});//document
